cecf1e8a383e6dd7b8f85d3a70ad06366947536f
Natan5533 <naelramosfilho@gmail.com>
2022-05-23 14:14:35 -0300

    implements book crud

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create book" do 
+    library = Library.create(name: "library", address: "avenida")
+    author = Author.create(name: "author", email: "a@a", library_id: library.id)
+    book = Book.new(title: "titulo", author_id: author.id, library_id: library.id)
+   
+    assert_equal book.save, true
+    assert_equal Book.find(book.id).title, "titulo"
+  end
+  test "Books belongs to one author" do
+    library = Library.create(id: 500, name: "library1", address: "test1")
+    author = Author.create(name: "Author", email: "b@b", library_id: library.id)
+    book = Book.new(title: "titulo", author_id: author.id, library_id: library.id)
+
+    assert_equal book.author.id, author.id
+  end
+  test "Books belongs to one library" do
+    library = Library.create(id: 500, name: "library1", address: "test1")
+    author = Author.create(name: "Author", email: "b@b", library_id: library.id)
+    book = Book.new(title: "titulo", author_id: author.id, library_id: library.id)
+
+    assert_equal book.library.id, library.id
+  end
 end
