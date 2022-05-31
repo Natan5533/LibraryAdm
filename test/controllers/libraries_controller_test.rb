d1cc2da0dabac6a403eadba1f7b14a0062cf59a0
Natan5533 <naelramosfilho@gmail.com>
2022-05-31 09:14:37 -0300

    tests controllers

---
+++ test/controllers/libraries_controller_test.rb
@@ -0,0 +1,49 @@
+
+class LibrariesControllerTest < ActionDispatch::IntegrationTest
+  #test "index" do
+   # get libraries_path
+   # assert_response 200
+  #end
+
+  test "show" do
+    library = libraries(:library_one)
+    get library_path(library.id)
+    assert_response :success
+  end
+  test "new" do
+    get new_library_path
+    assert_response :success
+  end
+  test "create" do 
+    post "/libraries",
+    params: { library: {name: "amazon", address: "rua sla" } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    library = Library.last
+     assert_equal library.name,"amazon"
+  end
+  test "edit " do 
+    library = libraries(:library_one)
+    get edit_library_path(library.id)
+    assert_response :success
+ end
+ test "update" do
+   library = Library.create(name: "amazon", address: "rua gripino")
+   put "/libraries/#{library.id}",
+   params: { library: { name: "Novo nome", address: "rua gripino" } }
+   assert_response :redirect
+   follow_redirect!
+   assert_response :success
+   library = Library.last
+   assert_equal library.name,"Novo nome"
+ end
+ test "delete" do
+  library = Library.create(name: "Delete", address: "rua test")
+  delete "/libraries/#{library.id}"
+  assert_response :redirect
+  follow_redirect!
+  assert_response :success
+ end
+ #testes de falha --
+end
--- test/models/library_test.rb
+++ test/models/library_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class LibraryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create library" do
+   library = Library.new(name: "kalunga", address: "Rua pinheiro")
+
+    assert_equal library.save, true
+    assert_equal Library.find(library.id).name, "kalunga"
+    assert_equal Library.find(library.id).address, "Rua pinheiro"
+  end
+  test "library has many authors" do
+    library = Library.create(name: "kalunga", address: "Rua pinheiro")
+    author1 = Author.create(name: "Jorge", email: "jorge@gmail", library_id: library.id)
+    author2 = Author.create(name: "Matheus", email: "Matheus@gmail", library_id: library.id)
+    authors = [author1, author2]
+
+    assert_equal library.authors,  authors
+  end 
+  test "library has many books" do
+    library = Library.create(name: "kalunga", address: "Rua pinheiro")
+    author = Author.create(name: "Jorge", email: "jorge@gmail", library_id: library.id)
+    book1 = Book.create(title: "titulo", author_id: author.id)
+    book2 = Book.create(title: "titulo 2", author_id: author.id)
+    books = [book1, book2]
+    assert_equal library.books, books
+  end 
+  test "empty name error" do
+    library = Library.create(address: "Rua pinheiro")
+    #require 'pry';  binding.pry
+   
+   # assert_equal library, false
+   assert library.errors.details == {:name=>[{:error=>:blank}]}
+  end
+  test "empty address error" do
+    library = Library.create(name: "Library")
+
+    assert library.errors.details == {:address=>[{:error=>:blank}]}
+  end
 end
