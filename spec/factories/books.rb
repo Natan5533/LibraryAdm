03893e01a2a0ebc463b0c95d2d7ee47bc3876eab
Natan5533 <naelramosfilho@gmail.com>
2022-06-18 17:34:01 -0300

    Creation of factories

---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :book do
+        trait :regular_book do
+            title {'First'}
+            association :author , :regular_author
+        end
+    end
+end
---
+++ spec/factories/libraries.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :library do
+        trait :regular_library do
+            name {'Banca'}
+            address {'1° avenue'}
+        end
+    end
+end
