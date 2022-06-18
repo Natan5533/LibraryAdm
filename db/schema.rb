1c00cc0fce3b8e55632f0d7901f1d33e54fe37f2
Natan5533 <naelramosfilho@gmail.com>
2022-06-18 17:29:20 -0300

    Changes in settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_114924) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_133835) do
   create_table "authors", force: :cascade do |t|
     t.integer "library_id"
     t.string "name"
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_114924) do
   end
 
   create_table "libraries", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
